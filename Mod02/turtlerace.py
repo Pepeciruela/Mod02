15e5e46bd8afda251473ea075c35b2cdbcd624b1
Angel <calvo.monzon.angel@gmail.com>
2020-11-03 16:17:08 +0100

    campo de juegos

--- Mod02/turtlerace.py
+++ Mod02/turtlerace.py
@@ -8,10 +8,25 @@ class Game():
     def __init__(self):
         self.__screen = pygame.display.set_mode((640,480))
         self.__background = pygame.image.load("images/background.png")
+        pygame.display.set_caption("Carrera de bichos")
         
-    
+    def competir(self):
+        gameOver = False
+        while not gameOver:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameOver = True
+                    
+            self.__screen.blit(self.__background,(0, 0))
+            
+            pygame.display.flip()
+        
+        pygame.quit()
+        sys.exit()
     
     
 if __name__ == "__main__":
     game = Game()
+    pygame.font.init()
+    game.competir()
     
