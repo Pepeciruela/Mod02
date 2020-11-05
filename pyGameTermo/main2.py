8801aa21631e755395c86034012a2ec4652024b0
Angel <calvo.monzon.angel@gmail.com>
2020-11-05 16:52:47 +0100

    nuevo

--- pyGameTermo/main2.py
+++ pyGameTermo/main2.py
@@ -3,94 +3,118 @@ from pygame.locals import *
 
 class Termometro():
     def __init__(self):
-        self.custome = pygame.image.load ("images/termo1.png")
+        self.custome = pygame.image.load("images/termo1.png")
         
     def convertir(self, grados, toUnidad):
         resultado = 0
-        if toUnidad == "F":
+        if toUnidad == 'F':
             resultado = grados * 9/5 + 32
-        elif toUnidad == "C":
-            resultado = (grados -32) * 5/9
+        elif toUnidad == 'C':
+            resultado = (grados - 32) * 5/9
         else:
             resultado = grados
-        
+            
         return "{:10.2f}".format(resultado)
+        
 
 class Selector():
     __tipoUnidad = None
     
-    def __init__(self, unidad ="C"):
+    def __init__(self, unidad="C"):
         self.__customes = []
         self.__customes.append(pygame.image.load("images/posiF.png"))
         self.__customes.append(pygame.image.load("images/posiC.png"))
         
         self.__tipoUnidad = unidad
-        
-    def custome (self):
-        if self.__tipoUnidad == "F":
+    
+    def custome(self):
+        if self.__tipoUnidad == 'F':
             return self.__customes[0]
         else:
             return self.__customes[1]
         
+        
     def change(self):
-        if self.__tipoUnidad == "F":
-            self.__tipoUnidad = "C"
+        if self.__tipoUnidad == 'F':
+            self.__tipoUnidad = 'C'
         else:
-            self.__tipoUnidad = "F"
-            
+            self.__tipoUnidad = 'F'
+        
     def unidad(self):
-        self.__tipoUnidad
+        return self.__tipoUnidad
+
+        
+        
     
-    
+
 class NumberInput():
     __value = 0
     __strValue = ""
     __position = [0, 0]
-    __size = [0, 0]
+    __size = [0,0]
     __pointsCount = 0
     
-    def __init__ (self, value=0):
+    def __init__(self, value=0):
         self.__font = pygame.font.SysFont("Arial", 24)
         self.value(value)
-        
+
+        '''
+        try:
+            self.__value = int(value)
+            self.__strValue = str(value)
+        except:
+            pass
+        '''
+
     def on_event(self, event):
         if event.type == KEYDOWN:
-            if event.unicode.isdigit() and len(self.__strValue) < 10 or (event.unicode == "." and self.__pointsCount ==0):
+            if event.unicode.isdigit() and len(self.__strValue) < 10 or (event.unicode == '.' and self.__pointsCount == 0):
                 self.__strValue += event.unicode
                 self.value(self.__strValue)
-                if event.unicode == ".":
+                if event.unicode == '.':
                     self.__pointsCount += 1
             elif event.key == K_BACKSPACE:
-                if self.__strValue[-1] == ".":
+                if self.__strValue[-1] == '.':
                     self.__pointsCount -= 1
                 self.__strValue = self.__strValue[:-1]
                 self.value(self.__strValue)
                 
-    def render (self):
+                
+    
+    def render(self):
         textBlock = self.__font.render(self.__strValue, True, (74, 74, 74))
         rect = textBlock.get_rect()
         rect.left = self.__position[0]
-        rect.top = self.__position [1]
+        rect.top = self.__position[1]
         rect.size = self.__size
-        
+
+        '''
+        return {
+                "fondo": rect,
+                "texto": textBlock
+            }
+        '''
         return (rect, textBlock)
         
-    def value(self, val = None):
-        if val == None:
+    
+    def value(self, val=None):
+
+        if val==None:
             return self.__value
         else:
             val = str(val)
+            print(val, "cadena")
             try:
                 self.__value = float(val)
                 self.__strValue = val
-                if "." in self.__strValue:
-                    self.__pointsCont = 1
+                if '.' in self.__strValue:
+                    self.__pointsCount = 1
                 else:
-                    sel.pointsCont = 0
+                    self.__pointsCount = 0
             except:
                 pass
-            
-    def widht(self, val=None):
+    
+    def width(self, val=None):
         if val == None:
             return self.__size[0]
         else:
@@ -98,7 +122,7 @@ class NumberInput():
                 self.__size[0] = int(val)
             except:
                 pass
-            
+
     def height(self, val=None):
         if val == None:
             return self.__size[1]
@@ -113,12 +137,10 @@ class NumberInput():
             return self.__size
         else:
             try:
-                w = int(val[0])
-                h = int(val[1])
                 self.__size = [int(val[0]), int(val[1])]
             except:
                 pass
-        
+
     def posX(self, val=None):
         if val == None:
             return self.__position[0]
@@ -127,7 +149,7 @@ class NumberInput():
                 self.__position[0] = int(val)
             except:
                 pass
-            
+
     def posY(self, val=None):
         if val == None:
             return self.__position[1]
@@ -144,7 +166,7 @@ class NumberInput():
             try:
                 self.__position = [int(val[0]), int(val[1])]
             except:
-                pass   
+                pass
 
 class mainApp():
     termometro = None
@@ -153,25 +175,27 @@ class mainApp():
     
     def __init__(self):
         self.__screen = pygame.display.set_mode((290, 415))
-        pygame.display.set_caption("Termometro")
+        pygame.display.set_caption("Termómetro")
+
         
         self.termometro = Termometro()
-        self.entrada = NumberInput()
+        self.entrada = NumberInput("Monchu")
         self.entrada.pos((106, 58))
         self.entrada.size((133, 28))
         
-        self.selector =Selector()
+        self.selector = Selector()
         
+    
     def __on_close(self):
         pygame.quit()
         sys.exit()
-
+    
     def start(self):
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
                     self.__on_close()
-                
+                    
                 self.entrada.on_event(event)
                 
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -181,28 +205,23 @@ class mainApp():
                     temperatura = self.termometro.convertir(grados, nuevaUnidad)
                     self.entrada.value(temperatura)
                     
-                
             #Pintamos el fondo de pantalla
             self.__screen.fill((244, 236, 203))
-            
-            #Pintamos el termometro en su posición
+                
+            #Pintamos el termómetro en su posición
             self.__screen.blit(self.termometro.custome, (50, 34))
             
             #Pintamos el cuadro de texto
-            text = self.entrada.render() #Obtenemos rectángulo y foto de texto y lo asignamos a text
-            pygame.draw.rect(self.__screen, (255, 255, 255), text[0]) #Creamos el rectangulo blanco con sus datos (posición y tamaño) text [0]
-            self.__screen.blit(text[1], self.entrada.pos()) #Pintamos la foto del text (text[1])
-            
+            text = self.entrada.render() # Obtenememos rectánculo blanco y foto de texto y lo asignamos a text
+            pygame.draw.rect(self.__screen, (255, 255, 255), text[0]) #creamos el rectángulo blanco con sus datos (posición y tamaño) text[0]
+            self.__screen.blit(text[1], self.entrada.pos()) #Pintamos la foto del texto (text[1])
+            '''
             #Pintamos el selector
-            self.__screen.blit(self.selector.custome(), (112, 153))
-            
-            
+            self.__screen.blit(self.selector.custome(), (112,153))
+            '''
             pygame.display.flip()
-                    
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     pygame.font.init()
     app = mainApp()
     app.start()
-    
